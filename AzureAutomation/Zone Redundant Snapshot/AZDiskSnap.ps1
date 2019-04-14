6729f8085cbd50bba1b940b60727224b7b07f013
Erick Moore <ermoor@microsoft.com>
2019-04-14 11:45:43 -0400

    moved to folder and addedd readme

---
+++ AzureAutomation/Zone Redundant Snapshot/AZDiskSnap.ps1
@@ -0,0 +1,62 @@
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+# //	Set script variables
+
+$tagToFind = 'DB2' # // Azure tag to match resources
+$location = 'eastus2' # // Azure region to search for VM's
+$snapRG = 'Backup' # // Resource group to hold snapshot
+$snapDate = Get-Date -f MM-dd-yyyy # // Set snapshot date
+$snapTime = Get-Date -f HH-mm # // Set snapshot time
+$snapshotName = "snap-time-$snapTime-date-$snapDate" # // Snapshot name
+$vmList = @() # // Re-initialize variable
+
+# //	Find all VM's in a region
+
+$allVMs = Get-AzVM -Location $location
+
+# // 	Loop through all VM's in a region to find matching tag
+
+ForEach ($vm in $allVMs) {
+
+	$tagTest = ($vm.Tags | where Values -eq $tagToFind) # // Find VM's with matching tag
+
+	if ($tagTest -ne $null) { $vmList += $vm } # // If tag is found add to VM list
+
+}
+
+# //	Loop through all matching VM's and create a zone redundant OS snapshot
+
+ForEach ($vm in $vmList) {
+
+	$snapshot =  New-AzSnapshotConfig -SourceUri $vm.StorageProfile.OsDisk.ManagedDisk.Id `
+		-Location $location `
+		-SkuName Standard_ZRS `
+		-CreateOption copy
+
+	New-AzSnapshot -Snapshot $snapshot `
+		-SnapshotName $snapshotName `
+		-ResourceGroupName $snapRG
+
+}
