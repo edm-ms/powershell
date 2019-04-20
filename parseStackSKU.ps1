3c3ed6fff198e1d4fbd046025ecd030526b31375
Erick Moore <erick.moore@microsoft.com>
2019-04-20 18:01:10 -0400

    added new fields for IOPS and drive counts

--- parseStackSKU.ps1
+++ parseStackSKU.ps1
@@ -10,16 +10,42 @@
     Eventually will ask for input file to match against Azure Stack SKUs
 
 .EXAMPLE
-    ./parseStackSKU.ps1 
+    parseStackSKU.ps1
+
+    Run this to store the global variable $skuReport.
+    Once this has been run use the examples below to retreive SKU data from the object.
+
+.EXAMPLE
+    $skuReport | where vCPU -gt 8 | select Size, vCPU, 'Memory (GiB)'
+
+    Find SKUs greater than 8 vCPU then select SKU Name, vCPU count, and Memory
+
+.EXAMPLE
+    $skuReport | where vCPU -le 4 | where 'Memory (GiB)' -ge 32 | select Size, vCPU, 'Memory (GiB)'
+
+    Find SKUs less than or equal to 4 vCPU and greater than or equal to 32GB memory, select SKU Name, vCPU count, and Memory
+
+.EXAMPLE
+    $skuReport | Measure-Object 'Memory (GiB)' -Average -Maximum -Minimum
+
+    Find maximum, minimum, and average memory values of all SKUs
+
+.OUTPUTS
+    $skuReport
 
 .NOTES
     Plan to add parameters for a few things like -MatchFile to match values in stack to a file.
 
 .LINK
-    Azure Stack SKUs: https://docs.microsoft.com/en-us/azure-stack/user/azure-stack-vm-sizes
+    https://docs.microsoft.com/en-us/azure-stack/user/azure-stack-vm-sizes
 
 #>
 
+[CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$false)]
+        [switch]$MatchFile
+)
 Function ConvertTo-NormalHTML {
     param([Parameter(Mandatory = $true, ValueFromPipeline = $true)]$HTML)
 
@@ -52,8 +78,6 @@ $stackHTML = ConvertTo-NormalHTML $stackSKU
 
 $tables = @($stackHTML.getElementsByTagName('TABLE'))
 
-# Disk IOP format --- need to break apart to new field |  8 / 8x500
-
 $titles = @()
 $global:skuReport = @()
 
@@ -71,7 +95,7 @@ foreach ($table in $tables) {
         # // Grab all cells in current row
         $cells = @($row.Cells)
 
-        # // Find title header and build names
+        # // If HTML tag is header loop through all cells to build field names
         if ($cells[0].tagName -eq "TH") { $titles = @($cells | % { ("" + $_.InnerText).Trim() }) }
 
         # // Move to next table in loop if retired SKU
@@ -91,17 +115,44 @@ foreach ($table in $tables) {
             # // Test if value can be an integer and cast as int if true
             if ( [bool]($skuData -as [int] -is [int]) -eq $true) { $skuData = [int]$skuData }
 
-            $skuObj | Add-Member -NotePropertyName $titles[$counter] -NotePropertyValue $skuData -Force
+            # // Reduce length of long title names
+            if ( $titles[$counter] -eq 'Memory (GiB)') { $titles[$counter] = 'Memory' }
+            if ( $titles[$counter] -eq 'Max OS disk throughput (IOPS)') { $titles[$counter] = 'MaxOsDriveIOPS' }
+            if ( $titles[$counter] -eq 'Max temp storage throughput (IOPS)') { $titles[$counter] = 'MaxTempDriveIOPS' }
+            if ( $titles[$counter] -eq 'Max NICs') { $titles[$counter] = 'NICs' }
 
-            $counter ++
+            # // Check if this is the combined data disk and IOPS field
+            # // if yes then split them and create 3 fields (max drive count, max single drive IOPS, max total IOPS)
+            if ( $titles[$counter] -eq 'Max data disks / throughput (IOPS)') { 
 
+                # // Check if field doesn't follow the same formating and fix it
+                if ( ($skuData.ToCharArray()) -notcontains '/' ) { $skuData = $skuData.split('x')[0] + ' / ' + $skuData }
+
+                $driveSplit = $skuData.Split('/')
+                $maxDrives = $driveSplit[0]
+                $maxDrives = [int]$maxDrives
+                $maxIOPS = $driveSplit[1].Trim()
+                $maxIOPS = $maxIOPS.Split('x')[1]
+                $maxIOPS = [int]$maxIOPS
+                $maxTotalIOPS = $maxDrives * $maxIOPS
+
+                $skuObj | Add-Member -NotePropertyName 'MaxDrives' -NotePropertyValue $maxDrives -Force
+                $skuObj | Add-Member -NotePropertyName 'MaxSingleDriveIOPS' -NotePropertyValue $maxIOPS -Force
+                $skuObj | Add-Member -NotePropertyName 'MaxTotalIOPS' -NotePropertyValue $maxTotalIOPS -Force
+
+                $counter ++
+
+            }
+            else {
+
+                $skuObj | Add-Member -NotePropertyName $titles[$counter] -NotePropertyValue $skuData -Force
+                $counter ++
+
+            }
+            
         }
 
         $global:skuReport += $skuObj
 
     }
 }
-
-Write-Host 'Type $skuReport to see available Azure Stack SKUs'
-Write-Host 'Example find SKUs with greater than 4 vCPU: $skuReport | where vCPU -gt 4'
-Write-Host 'Example find SKUs greater than or equal to 32GB RAM: $skuReport | where "Memory (GIB)" -ge 32'
