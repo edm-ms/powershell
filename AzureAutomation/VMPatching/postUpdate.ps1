176842342f661da5bd2a40229d1727cbf34c73a6
Erick Moore <ermoor@microsoft.com>
2019-11-07 10:01:22 -0500

    pre/post script to start/stop VMs to patch

---
+++ AzureAutomation/VMPatching/postUpdate.ps1
@@ -0,0 +1,40 @@
+# // Start Azure Automation Login Using Service Principal
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+# // Set script variables
+$automationAccountName = "VMUpdateAutomation"
+$automationAccountRG = "VMUpdateAutomation"
+$automationAccountVariableName = "preUpdateVMStatus"
+
+# // Retreive pre-update VM run status
+$allVMs = Get-AzAutomationVariable -Name $automationAccountVariableName -ResourceGroupName $automationAccountRG -AutomationAccountName $automationAccountName
+$allVMs = $allVMs.Value
+
+# Loop through all VM's and power off all VMs that were off before update
+ForEach ($vm in $allVMs) {
+
+    If ($vm.PowerState -ne 'VM running') { Stop-AzVM -Name $vm.Name -ResourceGroupName $vm.ResourceGroupName }
+
+}
---
+++ AzureAutomation/VMPatching/preUpdate.ps1
@@ -0,0 +1,42 @@
+# // Start Azure Automation Login Using Service Principal
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+# // Set script variables
+$automationAccountName = "VMUpdateAutomation"
+$automationAccountRG = "VMUpdateAutomation"
+$automationAccountVariableName = "preUpdateVMStatus"
+
+# // Find all VM's with power state
+$allVMs = Get-AzVm -Status
+
+# // Set Azure Automation variable with pre-update VM state
+New-AzAutomationVariable –AutomationAccountName $automationAccountName –Name $automationAccountVariableName –Encrypted $false -ResourceGroupName $automationAccountRG –Value $allVMs
+
+# Loop through all VM's and power on all that are off
+ForEach ($vm in $allVMs) {
+
+    If ($vm.PowerState -ne 'VM running') { Start-AzVM -Name $vm.Name -ResourceGroupName $vm.ResourceGroupName }
+
+}
