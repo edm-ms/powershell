7f4b3c26d2e9afb65935c824bc8d34ebaee4245f
Erick Moore <ermoor@microsoft.com>
2019-11-19 14:32:14 -0500

    fixed missing variable

--- VPN Gateway Policies/vpnForceTunnel.ps1
+++ VPN Gateway Policies/vpnForceTunnel.ps1
@@ -1,6 +1,8 @@
 $rgName = ""
 $vpnGwName = ""
+$localGwName = ""
 
-$LocalGateway = Get-AzLocalNetworkGateway -Name "DefaultSiteHQ" -ResourceGroupName $rgName
+$LocalGateway = Get-AzLocalNetworkGateway -Name $localGwName -ResourceGroupName $rgName
 $VirtualGateway = Get-AzVirtualNetworkGateway -Name $vpnGwName -ResourceGroupName $rgName
+
 Set-AzVirtualNetworkGatewayDefaultSite -GatewayDefaultSite $LocalGateway -VirtualNetworkGateway $VirtualGateway
